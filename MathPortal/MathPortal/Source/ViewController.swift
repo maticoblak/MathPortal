c5efd7ab742a390a6d1ee90093beaae1fabe038e
Petra Cackov <petra.cackov@gmail.com>
2019-04-12 08:02:13 +0200

    Basic project

---
+++ MathPortal/MathPortal/Source/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MathPortal
+//
+//  Created by Petra Čačkov on 11/04/2019.
+//  Copyright © 2019 Petra Čačkov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
